6967199601923b95abc2ae2e14623ea4b2e5ab4e
Rodrigo Huarcaya <r.huarcaya-alba@tu-braunschweig.de>
2018-04-25 21:01:37 +0200

    Checkstyle durchgeführt und Fehler in der Semantik korrigiert

--- Drachenkampf_F_.java
+++ Drachenkampf_F_.java
@@ -1,15 +1,32 @@
 /**
-	@Rodrigo Huarcaya Alba Y0091437
-	@Moritz
+ *	Hier Klassenkommentar schreiben:
+ *	Es soll die Klasse beschrieben.
+ *
+ *	@author Rodrigo Huarcaya Alba Y0091437
+ *	@author Moritz
+ *
+ *	@version Drachenspiels Version 1
+ *	
+ */
 
-	Drachenspiels Version 1
-**/
-
+ 
 
 import java.util.Scanner;
 
 public class Drachenkampf_F_ {
-	public static void main(String[] args){
+	public static void main(String[] args) {
+		/**
+		 *	@param heroLife: 	Leben des Heros
+		 *	@param dragonLife: 	Leben des Drachen
+		 *	@param powerPfeilBogen, powerSchwert, powerFeuer:	Angreifschaden der Waffen
+		 *	@param weaponChoice: Auswahl zwischen Waffen 
+		 *	@param heroRandomNum:	Zufalls generator für die Angriefswahrscheinlichkeit
+		 *
+		 *
+		 *	@return heroAttackPower: Angriefschaden nach Waffen auswahl
+		 *	@return heroAttackChance: Hero Angriefserfolgswahrscheinlichkeit nach Waffen auswahl
+		 *	@return heroLife, dragonLife: Falls ein Angreif Erfolgreich war, reduzierte Leben wiedergeben
+		 */
 		//Methode um ein neues Scanner hinzuzufügen
 		Scanner s = new Scanner(System.in);
 		
@@ -17,8 +34,8 @@ public class Drachenkampf_F_ {
 		int dragonLife = 15;
 		
 		// Mit Hilfe von new char und replace wird das Leben des Helden bzw. des Drachen von int auf String umgewandelt
-		String heroLifeO = new String(new char [heroLife]).replace("\0", "O ");		// new char has a dafault value 0 oder \u0000 oder \0
-		String dragonLifeX = new String(new char [dragonLife]).replace("\0", "X "); 
+		String heroLifeO = new String(new char[heroLife]).replace("\0", "O ");		// new char has a dafault value 0 oder \u0000 oder \0
+		String dragonLifeX = new String(new char[dragonLife]).replace("\0", "X "); 
 		System.out.println("");
 		System.out.println("Der Kamfp beginnt");
 		System.out.println("Leben des Helden	:" + heroLifeO);	// Leben des Heldes ausgegeben
@@ -32,32 +49,32 @@ public class Drachenkampf_F_ {
 		do {
 			System.out.println("");
 			System.out.println("Der Hedl kann mit Pfeil und Bogen (1) oder mit dem Schwert (2) angreifen.");
-			int weaponChoice = s.nextInt();//(1) Für Pfeil und Bogen/(2) für Schwert
+			int weaponChoice = s.nextInt(); //(1) Für Pfeil und Bogen/(2) für Schwert
 			// Paramater des Helden werden erzeugt bzw. berechnet
 			int heroAttackPower = 0;
-			int heroRandomNum = (int)(Math.random()*10);
+			int heroRandomNum = (int) (Math.random() * 10);
 			int heroAttackChance = 0;
 			
 			switch (weaponChoice) {
 				//Pfeil wurde ausgewählt
-			case 1:	
-				heroAttackPower = powerPfeilBogen;
-				heroAttackChance = 5;
-				System.out.println(heroAttackChance);
-				break;
-			case 2:
-				//Schwert wurde ausgewählt
-				heroAttackPower = powerSchwert;
-				heroAttackChance = 3;
-				System.out.println(heroAttackChance);
-				break;
-			default:
-				System.out.println("Sie haben eine ungueltige Option gewaehlt");
+				case 1:	
+					heroAttackPower = powerPfeilBogen;
+					heroAttackChance = 5;
+					System.out.println(heroAttackChance);
+					break;
+				case 2:
+					//Schwert wurde ausgewählt
+					heroAttackPower = powerSchwert;
+					heroAttackChance = 3;
+					System.out.println(heroAttackChance);
+					break;
+				default:
+					System.out.println("Sie haben eine ungueltige Option gewaehlt");
 			}
 			
 			// Parameter des Drachen werden erzeugt bzw. berechnet
 			int dragonAttackPower = powerFeuer;
-			int dragonRandomNum = (int)(Math.random()*10);
+			int dragonRandomNum = (int) (Math.random() * 10);
 			int dragonAttackChance = 4;
 			/*Bis Hier werden alle Parameter für den Held und den Drachen berechnet
 			--------------------------*/
@@ -81,9 +98,9 @@ public class Drachenkampf_F_ {
 				System.out.println("Der Drache hatte eine Fehlzuendung. Glueck gehabt");
 			}
 			
-			if (dragonLife >= 0 && heroLife >=0){
-				heroLifeO = new String(new char [heroLife]).replace("\0", "O ");
-				dragonLifeX = new String(new char [dragonLife]).replace("\0", "X ");
+			if (dragonLife >= 0 && heroLife >= 0) {
+				heroLifeO = new String(new char[heroLife]).replace("\0", "O ");
+				dragonLifeX = new String(new char[dragonLife]).replace("\0", "X ");
 				System.out.println("Leben des Helden	:" + heroLifeO);	// Leben des Heldes ausgegeben
 				System.out.println("Leben des Drachen	:" + dragonLifeX);	// Leben des Drachen ausgegeben
 			}
