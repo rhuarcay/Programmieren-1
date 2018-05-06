7616480f01c3617c8070870f48fc134ccc7fa463
Rodrigo Huarcaya <r.huarcaya-alba@tu-braunschweig.de>
2018-05-06 12:33:45 +0200

    Code zum Einlesen, Testen und Validieren der Parameter eingefügt. Zur Validierung ist die Methode(isInterger) zuständig. Methode für die Aufzeichnung des Feldes hinzugefügt (drawArena)

--- DrachenKampf.java
+++ DrachenKampf.java
@@ -12,14 +12,43 @@ public class DrachenKampf {
      * @param args Bisher keine
      */
     public static void main(String[] args) {
-        String t = "\u2665"; //Herz
+        String[] sParam = args;
+		if (sParam.length != 2) {
+				System.out.println("Sie haben eine fehlerhafte anzahl an Parameter eingegeben \nDas Programm wird abgebrochen");
+				System.exit(1);
+			}
+			
+			int feldWide = 0; // Variable für die Breite des Feldes
+			int feldLength = 0; // Variable für die Länge des Feldes
+			int [][] playerPos = new int [0][0]; // Variable für die Start-Pos des Helden
+			int [][] dragonPos = new int [0][0]; // Variable für die Start-Pos des Drachen
+			char [][] arenaC = new char [0][0];
+			
+			if (isInterger(sParam[0]) && isInterger(sParam[1])) {
+				feldWide = Integer.parseInt(sParam[0]);
+				feldLength = Integer.parseInt(sParam[1]);
+				//System.out.println(feldWide + "" + feldLength + "Wurden in Int umgewandelt");
+			} else {
+				System.out.println("Sie haben eine nicht güiltige Parameter eingegeben \nBitte geben Sie nur Zahlen als Parameter ein");
+				System.exit(2);
+			}
+			
+			if (feldLength == feldWide && feldLength >= 9 && feldLength % 2 != 0) {
+				playerPos = new int [feldLength/2][1];
+				dragonPos = new int [feldLength/2][feldWide-1];
+				arenaC = new char [feldLength][feldWide];
+				drawArena(arenaC);
+			} else {
+				System.out.println("Bitte überprüfen Sie die Parameter nochmal \nDie Parameter sollen gleich sein,  >= 9 sein \nund sollen ungerade sein");
+			}
+		
+		String t = "\u2665"; //Herz
         int player = 12; //Leben des Helds
         int dragon = 6; //Leben des Drachen
 
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Der Kampf beginnt");
-
+		
+		
         while (true) {
             int playerATK = 3; //Angriff des Spielers, jedesmal zurückgesetzt
             int dragonATK = 4; //Angriff des Drachen
@@ -77,4 +106,37 @@ public class DrachenKampf {
             }
         }
     }
+	
+	public static boolean isInterger(String input) {
+		try {
+			Integer.parseInt(input);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+	
+	public static void drawArena(char[][] arena) {
+		for (int i = 0; i < arena.length; i++) {
+			for (int j = 0; j < arena.length; j++) {
+				arena[i][j] = '#';
+				if (i==0) {
+					System.out.print(arena[i][j]);
+				} else if (i==arena.length-1) {
+					System.out.print(arena[i][j]);
+				} else {
+					if (j ==0) {
+						System.out.print(arena[i][j]);
+					} else if (j==arena.length-1) {
+						System.out.print(arena[i][j]);
+					} else {
+						System.out.print(" ");
+					}
+					
+				}
+			}
+			System.out.print("\n");
+		}
+	}
+	
 }
